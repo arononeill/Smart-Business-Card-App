c41beac48ebc6b40652da65fb6f510fcc84d87b8
arononeill <arononeill98@gmail.com>
2021-02-27 14:11:43 +0000

    OuterContainer Skeleton Code & YouTube Tutorial Notes

---
+++ react_youtube_tutorial/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import OuterContainer from './OuterContainer'
+import 'rsuite/dist/styles/rsuite-default.css';
+import { Container, Header, Content, Footer, Sidebar, Button } from 'rsuite';
+
+function App() {
+  return (
+    <div className="App">
+      <OuterContainer/>
+    </div>
+  );
+}
+
+export default App;
